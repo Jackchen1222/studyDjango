195bc75fb650bd0150f4c85270dd9ab6892492c0
Jackchen1222 <824936026@qq.com>
2019-09-16 06:24:25 +0800

    上传初始代码

---
+++ blog/admin.py
@@ -0,0 +1,80 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.urls import reverse
+from django.utils.html import format_html
+
+from .models import Post,Category,Tag
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ( 'name', 'status', 'is_nav', 'created_time',)
+    fields = ( 'name', 'status', 'is_nav',)
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(CategoryAdmin, self).save_model(request, obj, form, change)
+    def post_count(self, obj):
+        return obj.post_set.count()
+
+    post_count.short_description = '文章数量'
+
+    def __str__(self):
+        return self.name
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ( 'name', 'status', 'created_time', )
+    fields = ( 'name', 'status', )
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(TagAdmin, self).save_model(request, obj, form, change)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'title', 'category', 'status',
+        'created_time', 'operator',
+    ]
+    list_display_links = []
+    list_filter = [ 'category', ]
+    search_fields = [ 'title', 'category__name', ]
+
+    actions_on_top = True
+    actions_on_bottom = True
+
+    #编辑页面
+    save_on_top = True
+
+    fields = (
+        ( 'category', 'title', ),
+        'desc',
+        'status',
+        'content',
+        'tag',
+    )
+    def operator(self, obj):
+        return format_html(
+            '<a href="{}">编辑</a>',
+            reverse('admin:blog_post_change', args=(obj.id,))
+        )
+        operator().short_description = '操作'
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(PostAdmin, self).save_model(request, obj, form, change)
+
+class CategoryOwnerFilter(admin.SimpleListFilter):
+    """ 自定义过滤器只展示当前用户分类"""
+    title = '分类过滤器'
+    parameter_name = 'owner_category'
+
+    def lookups(self, request, model_admin):
+        return Category.objects.filter(owner=request.user).values_list( 'id', 'name', )
+
+    def queryset(self, request, queryset):
+        category_id = self.value()
+        if category_id:
+            return queryset.filter(category_id=self.value())
+        return queryset
---
+++ studyDjango/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for studyDjango project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studyDjango.settings')
+
+profile = os.environ.get('TYPEIDEA_MODULE', 'typeidea.settings')
+
+application = get_wsgi_application()
